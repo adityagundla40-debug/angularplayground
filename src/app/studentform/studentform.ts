69b6176f4a04d807caecbae8ad4cd60e1d9dde51
Aditya Gundla <adityagundla08@gmail.com>
2026-02-04 13:19:40 +0530

    tried to add error message of filed not filled

--- src/app/studentform/studentform.ts
+++ src/app/studentform/studentform.ts
@@ -9,6 +9,16 @@ import {FormGroup,FormControl, ReactiveFormsModule} from '@angular/forms';
 })
 export class Studentform {
 
+  //arr: Array<String> = [];
+
+  pagedata = {
+    name: '',
+    phone: '',
+    gender: '',
+    address: '',
+    emptyfield: [] as String[]
+  };
+
   searchvalues: FormGroup = new FormGroup({
     'stuname': new FormControl(''),
   'stuphone': new FormControl(''),
@@ -20,9 +30,40 @@ export class Studentform {
 
 
   handlestu(){
-    console.log(this.searchvalues.get('stuname')?.value);
-    console.log(this.searchvalues.get('stuphone')?.value);
-    console.log(this.searchvalues.get('stugender')?.value);
-    console.log(this.searchvalues.get('stuaddress')?.value);
+
+    this.pagedata.emptyfield = [];
+    
+    for(let i = 0; i < 4; i++){
+      switch(i){
+
+        case 0:
+          if(this.searchvalues.get('stuname')?.value?.trim() == ''){
+            this.pagedata.emptyfield.push("name");
+          }
+          break;
+        case 1:
+          if(this.searchvalues.get('stuphone')?.value?.trim() == ''){
+            this.pagedata.emptyfield.push("phone");
+          }
+          break;
+        case 2:
+          if(this.searchvalues.get('stugender')?.value?.trim() == ''){
+            this.pagedata.emptyfield.push("gender");
+          }
+          break;
+        case 3:
+          if(this.searchvalues.get('stuaddress')?.value?.trim() == ''){
+            this.pagedata.emptyfield.push("address");
+          }
+          break;
+      }
+    }
+    
+    this.pagedata.name = this.searchvalues.get('stuname')?.value;
+    this.pagedata.phone = this.searchvalues.get('stuphone')?.value;
+    this.pagedata.gender = this.searchvalues.get('stugender')?.value;
+    this.pagedata.address = this.searchvalues.get('stuaddress')?.value;
+
+
   } 
 }
